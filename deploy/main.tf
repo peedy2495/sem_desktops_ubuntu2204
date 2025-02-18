5401bc8bafb4386c00670c6a8156b50fe28a4cd9
Peedy2495 <mail.petermark@web.de>
2025-02-18 14:17:02 +0100

    testing terraform: new network target

--- deploy/main.tf
+++ deploy/main.tf
@@ -34,8 +34,8 @@ variable "ssh_key" {
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "test_server" {
-  count = 1 # just want 1 for now, set to 0 and apply to destroy VM
-  name = "test-vm-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+  #count = 1 # just want 1 for now, set to 0 and apply to destroy VM
+  name = "test-vm-1" #${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
 
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
   target_node = "pve"
@@ -91,7 +91,7 @@ resource "proxmox_vm_qemu" "test_server" {
   # be 10.98.1.91 since count.index starts at 0. this is how you can create
   # multiple VMs and have an IP assigned to each (.91, .92, .93, etc.)
 
-  ipconfig0 = "ip=192.168.122.100+${format("192.168.122.%d", 100 + count.index + 1)}/24,gw=192.168.122.1"
+  ipconfig0 = "ip=192.168.124.100/24,gw=192.168.122.1"
   
   # sshkeys set using variables. the variable contains the text of the key.
   sshkeys = <<EOF
