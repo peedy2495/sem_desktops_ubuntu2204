6c19d45e5746b2534d94e01eea38ba5b8c6301b1
Peedy2495 <mail.petermark@web.de>
2025-02-19 07:53:59 +0100

    debugging terraform

--- deploy/readout.tf
+++ deploy/readout.tf
@@ -1,27 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "3.0.1-rc6"
-    }
-  }
-}
-
-variable "api_token" {
-  description = "API authentication token"
-  sensitive   = true  # Optional: mark as sensitive if needed
-}
-
-variable "ssh_key" {
-  description = "SSH pubclic key"
-  sensitive   = true  # Optional: mark as sensitive if needed
-}
-
-variable "ci_user_password" {
-  description = "administrative password"
-  sensitive   = true  # Optional: mark as sensitive if needed
-}
-
 data "http" "vm_info" {
   url = "https://192.168.124.2:8006/api2/json"
 
