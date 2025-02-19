4f8dd7057a3e03da742c0b012429846b9623120c
Peedy2495 <mail.petermark@web.de>
2025-02-19 08:03:26 +0100

    debugging terraform

--- deploy/readout.tf
+++ deploy/readout.tf
@@ -1,5 +1,8 @@
-data "proxmox" "vm_info" {
-  path = "/nodes/pve/qemu/100/config"
+data "http" "vm_info" {
+  url = "https://192.168.124.2:8006/api2/json/nodes/pve/qemu/100/config"
+  request_headers = {
+    Authorization = "PVEAPIToken=root@pam!semaphore=${var.api_token}"
+  }
 }
 
 output "vm_info" {
